92c84ca50e532670a9865676f052b56bd217faa3
shimonah <provodnik357@gmail.com>
2016-12-13 20:17:33 +0200

    Update install-lamp-server.sh

--- lamp/install-lamp-server.sh
+++ lamp/install-lamp-server.sh
@@ -1,6 +1,32 @@
+# This code for Ubuntu 14.04 LTS
 sudo apt-get update
 
 #Easy way to install Lamp server. For Ubuntu 14.04 LTS it install php 5.5.9
 sudo apt-get install lamp-server^
 
 #Separate installing each element
+
+# PHP 5.6
+# First of all php
+sudo add-apt-repository ppa:ondrej/php5-5.
+# If you get an error here, you need to install python-software-properties first (and then do the line above again):
+sudo apt-get update
+sudo apt-get install python-software-properties
+# Update
+sudo apt-get update
+# Install php 5.6
+sudo apt-get install php5
+# Check
+php -v
+
+#PHP 7.0
+sudo add-apt-repository ppa:ondrej/php
+# If you get an error here, you need to install python-software-properties first (and then do the line above again):
+sudo apt-get update
+sudo apt-get install python-software-properties
+# Update
+sudo apt-get update
+# Install
+sudo apt-get install php
+# Check
+php -v
