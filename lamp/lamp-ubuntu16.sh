a9e7b0358bb36c7c7d27f587344e9c95885d2498
shimonah <provodnik357@gmail.com>
2016-12-13 21:36:12 +0200

    Create lamp-ubuntu16.sh

---
+++ lamp/lamp-ubuntu16.sh
@@ -0,0 +1,19 @@
+# This code for Ubuntu 16.04 LTS
+# Easy installing
+sudo apt-get update
+sudo apt-get install lamp-server^
+
+# From the Ubuntu 16.04 can use 'apt' command in place of the traditional 'apt-get' and 'apt-cache'
+
+# Installing Apache2
+sudo apt install apache2
+
+# Installing MySQL
+sudo apt install mysql-server
+
+# Installing PHP
+# Ubuntu 16.04's PHP packages now default to v7.0. 
+sudo apt install php libapache2-mod-php php-mcrypt php-mysql
+
+# Installing phpMyAdmin
+sudo apt install phpmyadmin php-mbstring php-gettext
