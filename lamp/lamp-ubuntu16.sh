d5eed412f47a0fe0c8ecb203d1160f30f7749ac2
shimonah <provodnik357@gmail.com>
2016-12-13 22:22:16 +0200

    Update lamp-ubuntu16.sh

--- lamp/lamp-ubuntu16.sh
+++ lamp/lamp-ubuntu16.sh
@@ -1,19 +1,32 @@
 # This code for Ubuntu 16.04 LTS
 # Easy installing
-sudo apt-get update
-sudo apt-get install lamp-server^
+sudo apt update
+sudo apt install lamp-server^
 
 # From the Ubuntu 16.04 can use 'apt' command in place of the traditional 'apt-get' and 'apt-cache'
 
 # Installing Apache2
 sudo apt install apache2
 
-# Installing MySQL
-sudo apt install mysql-server
-
-# Installing PHP
+# !!! CODE BELOW NEEDS SOME ADDS !!!
+# Installing PHP 
 # Ubuntu 16.04's PHP packages now default to v7.0. 
 sudo apt install php libapache2-mod-php php-mcrypt php-mysql
+
+# PHP 5.6 
+sudo add-apt-repository ppa:ondrej/php
+# If you get an error here, you need to install python-software-properties first (and then do the line above again):
+sudo apt update
+sudo apt install python-software-properties
+# Update with new packages
+sudo apt update
+# Install php 5.6
+sudo apt -y install php5.6 php5.6-mcrypt php5.6-mbstring php5.6-curl php5.6-cli php5.6-mysql php5.6-gd php5.6-intl php5.6-xsl php5.6-zip
+# Check
+php -v
+
+# Installing MySQL
+sudo apt install mysql-server
 
 # Installing phpMyAdmin
 sudo apt install phpmyadmin php-mbstring php-gettext
