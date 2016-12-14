c6037e3f86f07760a86c308b9e58de1ee40fd1b4
shimonah <provodnik357@gmail.com>
2016-12-15 01:34:02 +0200

    Create sublime-text.sh

---
+++ dev-tools/sublime-text.sh
@@ -0,0 +1,7 @@
+#Auto install Sublime-Text-3
+# Add repository
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+# Update packages
+sudo apt-get update
+# Install Sublime-Text-3
+sudo apt-get install sublime-text-installer
