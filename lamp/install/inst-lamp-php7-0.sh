dbcc0ec0e976d7ff204e0120133433b538fd9599
shimonah <provodnik357@gmail.com>
2016-12-15 01:20:00 +0200

    Update inst-lamp-php7-0.sh

--- lamp/install/inst-lamp-php7-0.sh
+++ lamp/install/inst-lamp-php7-0.sh
@@ -34,7 +34,7 @@ sudo chmod -R 0755 phpmyadmin
 sudo chmod -R 775 /var/www/html
 # Notice
 echo '
-Thing you must do your self:
+Things you must do your self:
 Keep this tab for notice and open a new one Ctr+Alt+T
 # Configure apache2 file
 sudo nano /etc/apache2/mods-available/dir.conf
