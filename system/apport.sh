bd9319f76b42405389a91019d486ead522f210c7
shimonah <provodnik357@gmail.com>
2016-12-23 11:08:24 +0200

    Create apport.sh

---
+++ system/apport.sh
@@ -0,0 +1,10 @@
+# To disable 'system programm problem detected' after login
+# You can do this in two ways
+# First go to
+cd /var/crash
+# and delete all log files
+sudo rm *
+# Second you can disable this function showing this window at all
+sudo nano /etc/default/apport
+# and rewrite
+enabled=0
