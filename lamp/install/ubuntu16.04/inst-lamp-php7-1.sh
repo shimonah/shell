2759c5c0f1d2348e8d1fdd22bcc6aee8b972691f
shimonah <provodnik357@gmail.com>
2017-01-03 14:33:00 +0200

    Update inst-lamp-php7-1.sh

--- lamp/install/ubuntu16.04/inst-lamp-php7-1.sh
+++ lamp/install/ubuntu16.04/inst-lamp-php7-1.sh
@@ -1,22 +1,22 @@
 # Auto install Lamp server with PHP version 7.1
-sudo apt-get update
+sudo apt update
 # Install Apache2
-sudo apt-get install -y apache2
+sudo apt install -y apache2
 # Enabling mod_rewite
 sudo a2enmod rewrite
 sudo service apache2 restart
 # Installing PHP 7.1
 # Adding repository
 sudo add-apt-repository ppa:ondrej/php
-sudo apt-get update
-sudo apt-get install -y php7.1 php7.1-common
+sudo apt update
+sudo apt install -y php7.1 php7.1-common
 # Install the additional packages
-sudo apt-get install -y php7.1-curl php7.1-xml php7.1-zip php7.1-gd php7.1-mysql php7.1-mbstring
+sudo apt install -y php7.1-curl php7.1-xml php7.1-zip php7.1-gd php7.1-mysql php7.1-mbstring
 # Test PHP version
 php -v
 sudo service apache2 restart
 #Installing MySQL
-sudo apt-get install -y mysql-server
+sudo apt install -y mysql-server
 # Provide a new mysql root password when asked. Then restart the server
 sudo service apache2 restart
 # Install phpmyadmin
