4e54791699b5cf46443d35518b906bb1fee8cb70
Alexey Suchalkin <provodnik357@gmail.com>
2017-02-12 14:25:26 +0200

    Update true-inst-magento2.sh

--- magento/true-inst-magento2.sh
+++ magento/true-inst-magento2.sh
@@ -13,3 +13,15 @@ export APACHE_RUN_GROUP=www-data
  <Directory "/var/www/html/magento2">
                 AllowOverride All
  </Directory>
+ 
+# settings for installing magento2
+# enter your magento2 directory
+# example cd /var/www/html/magento2
+# set ownership
+sudo chown -R :www-data .
+# Set permissions:
+sudo find . -type d -exec chmod 770 {} \; && sudo find . -type f -exec chmod 660 {} \; && sudo chmod u+x bin/magento
+# go to
+cd /var/www/html/magento2
+# set permissions
+sudo chown -R alexey:www-data magento2
