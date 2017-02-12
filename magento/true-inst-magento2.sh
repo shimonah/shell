c956a949a6782d07aae33d80ac9b0a69a6830742
Alexey Suchalkin <provodnik357@gmail.com>
2017-02-12 14:08:22 +0200

    Update true-inst-magento2.sh

--- magento/true-inst-magento2.sh
+++ magento/true-inst-magento2.sh
@@ -1,6 +1,15 @@
+#add user to www-data group where 'alexey' is username
+sudo usermod -a -G www-data alexey
+
 # go to
 cd /etc/apache2/envvars
 
-# change to your user name and group name
-# export APACHE_RUN_USER=www-data
-# export APACHE_RUN_GROUP=www-data
+# change to your username
+# where 'alexey' is username and 'www-data' is group name
+export APACHE_RUN_USER=alexey
+export APACHE_RUN_GROUP=www-data
+
+# apache2 magento2.conf file settings
+ <Directory "/var/www/html/magento2">
+                AllowOverride All
+ </Directory>
