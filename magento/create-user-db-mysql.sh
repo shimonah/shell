1aba256e958071bf77854078f16fe7e320339848
Alexey Suchalkin <provodnik357@gmail.com>
2017-02-14 17:40:57 +0200

    Delete create-user-db-mysql.sh

--- magento/create-user-db-mysql.sh
+++
@@ -1,8 +0,0 @@
-
-# enter mysql from command line where root is user
-mysql -u root -p
-
-# creating user 'mage' and 'magento_dev' database 
-mysql> CREATE USER 'mage'@'localhost' IDENTIFIED BY 'password';
-mysql> CREATE DATABASE magento_dev;
-mysql> GRANT ALL PRIVILEGES ON magento_dev.* TO 'mage'@'localhost';
