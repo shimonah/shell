0d410178fc10f4543bbbb1f29f678aed925408fa
Alexey Suchalkin <provodnik357@gmail.com>
2017-04-27 11:06:07 +0300

    Create create-admin-user.sh

---
+++ magento/create-admin-user.sh
@@ -0,0 +1,1 @@
+php bin/magento admin:user:create --admin-user='alexey' --admin-password='pass' --admin-firstname='Alexey' --admin-email='alexeysuchalkin@m-integration.com' --admin-lastname='Sychalkin'
