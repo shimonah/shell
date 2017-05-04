af6fdcb198a4409f046143e684f1fb18847f6aaf
Alexey Suchalkin <provodnik357@gmail.com>
2017-05-04 14:50:26 +0300

    Create basic-auth.sh

---
+++ apache/basic-auth.sh
@@ -0,0 +1,27 @@
+# edit apache file
+sudo nano /etc/apache2/apache2.conf
+
+# add lines
+<Directory /var/www/html>
+        Options Indexes FollowSymLinks
+        AllowOverride All
+</Directory>
+
+# go to
+cd /var/www/html
+
+# create login/password
+sudo htpasswd -c .htpasswd username
+# you need to enter password for username(login)
+
+# then create file
+touch .htaccess
+
+# edit file, add lines
+AuthType Basic
+AuthName "Private Area"
+AuthUserFile /var/www/html/.htpasswd
+Require valid-user
+
+# restart apache server
+sudo systemctl restart apache2
