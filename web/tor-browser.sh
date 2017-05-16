124c61da7db701291142a09d75490ab8853aec62
Alexey Suchalkin <provodnik357@gmail.com>
2017-05-16 23:10:36 +0300

    Create tor-browser.sh

---
+++ web/tor-browser.sh
@@ -0,0 +1,9 @@
+#Установка Tor Browser из PPA.
+#Сначала добавьте PPA:
+sudo add-apt-repository ppa:webupd8team/tor-browser
+
+#Обновите списки пакетов:
+sudo apt update
+
+#Теперь установка Tor Browser:
+sudo apt install tor-browser
