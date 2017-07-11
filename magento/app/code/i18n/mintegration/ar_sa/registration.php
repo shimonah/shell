9b050ea9b0580bfbd545ea9c62615f6a4a1fe32b
Alexey Suchalkin <provodnik357@gmail.com>
2017-07-11 15:00:57 +0300

    Create registration.php

---
+++ magento/app/code/i18n/mintegration/ar_sa/registration.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'mintegration_ar_sa',
+    __DIR__
+);
