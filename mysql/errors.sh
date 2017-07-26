bfa559ec763a45dece5786ea2a38de1fc3838e3d
Alexey Suchalkin <provodnik357@gmail.com>
2017-07-26 11:08:20 +0300

    Update errors.sh

--- mysql/errors.sh
+++ mysql/errors.sh
@@ -2,3 +2,10 @@
 # Connect to musql database via console and execute the command:
 
 SET GLOBAL show_compatibility_56 = ON;
+# or
+sudo nano /etc/mysql/conf.d/mysql.cnf
+
+#Then I added this section:
+[mysqld]
+show_compatibility_56 = ON
+performance_schema
