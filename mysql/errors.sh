4682aae7be22a9ceae92d27c9b11b6125f3cd099
Alexey Suchalkin <provodnik357@gmail.com>
2017-07-26 11:04:23 +0300

    Create errors.sh

---
+++ mysql/errors.sh
@@ -0,0 +1,4 @@
+# ERROR 3167 (HY000): The 'INFORMATION_SCHEMA.GLOBAL_VARIABLES' feature is disabled; see the documentation for 'show_compatibility_56'
+# Connect to musql database via console and execute the command:
+
+SET GLOBAL show_compatibility_56 = ON;
