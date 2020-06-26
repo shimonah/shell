014b50c16594dcf560856c5434301ea622aecc5f
shimonah <provodnik357@gmail.com>
2020-06-26 16:37:40 +0300

    Update xdebug.sh

--- Phpstorm/xdebug.sh
+++ Phpstorm/xdebug.sh
@@ -7,17 +7,12 @@ sudo service apache2 restart
 
 #edit file
 nano /etc/php/7.0/mods-available/xdebug.ini
-#add this line
-xdebug.show_error_trace = 1
+#add this line after zend_extension=xdebug.so
 xdebug.remote_enable=on
-xdebug.default_enable=on
-xdebug.remote_autostart=on
-xdebug.remote_port=9000
-xdebug.remote_host=localhost
-xdebug.profiler_enable_trigger=1
-xdebug.profiler_output_name=xdebug-profile-cachegrind.out-%H-%R
-xdebug.var_display_max_children = 128
-xdebug.var_display_max_data = 4096
-xdebug.var_display_max_depth = 128
-xdebug.max_nesting_level = 500
-xdebug.idekey=PHPSTORM
+xdebug.remote_connect_back=1
+xdebug.remote_port = 9000
+xdebug.remote_autostart=1
+xdebug.scream = 0
+xdebug.cli_color = 1
+xdebug.show_local_vars = 1
+xdebug.idekey=XDEBUG_ECLIPSE
